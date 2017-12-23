7f09976faeea08b5e7f8a58f9e549e17aafa8f64
maksim.skavysh <maksim.skavysh@gmail.com>
2017-12-24 02:42:14 +0300

    fix

--- hw12/main.py
+++ hw12/main.py
@@ -48,15 +48,14 @@ X_r = pca.fit(X).transform(X)
 print('explained variance ratio (first two components): %s'
       % str(pca.explained_variance_ratio_))
 
-
 plt.figure()
-colors = ['navy', 'turquoise', 'darkorange']
+colors = ['red', 'blue', 'green']
 lw = 2
 
 for color, i, target_name in zip(colors, [0, 1, 2], target_names):
     plt.scatter(X_r[y == i, 0], X_r[y == i, 1], color=color, alpha=.8, lw=lw,
                 label=target_name)
-plt.legend(loc='best', shadow=False, scatterpoints=1)
-plt.title('PCA of IRIS dataset')
+# plt.legend(loc='best', shadow=False, scatterpoints=1)
+plt.title('PCA')
 
 plt.show()
